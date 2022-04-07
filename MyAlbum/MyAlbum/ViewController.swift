8fe77abfe336c5f4156600d8674bf0874509dd57
pycqkrdyd <101839207+pycqkrdyd@users.noreply.github.com>
2022-04-08 07:16:13 +0900

    FastCampus_iOSAppDevelopmentAllInOnePackage_2
    https://blog.naver.com/qkrdydpyc/222694872597

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -12,14 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        priceLabel.text = "₩ \(currentValue)"
     
+    }
+    var currentValue = 0
+    
+    @IBOutlet weak var priceLabel: UILabel!
     
     @IBAction func NewChallenge(_ sender: Any) {
-        let NewAlert = UIAlertController(title: "도전과제", message: "할수있다", preferredStyle: .alert)
+        let message = "가격은 ₩\(currentValue)입니다."
+        let NewAlert = UIAlertController(title: "도전과제", message: message, preferredStyle: .alert)
         let NewAction = UIAlertAction(title: "도전!", style: .default, handler: nil)
         NewAlert.addAction(NewAction)
         present(NewAlert, animated: true, completion: nil)
+        
+        let randomPrice = arc4random_uniform(10000) + 1
+        
+        priceLabel.text = "₩ \(currentValue)"
+        currentValue = Int(randomPrice)
+        
     }
 }
 
